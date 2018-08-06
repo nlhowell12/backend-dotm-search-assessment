13870e133fbbd0143ce16b6c47b58a9803963d30
Nick Howell <nlhowell12@gmail.com>
2018-08-06 13:26:18 -0400

    assignment complete

--- dotm_search.py
+++ dotm_search.py
@@ -5,3 +5,33 @@ within the 'word/document.xml' section of a MSWord .dotm file.
 """
 
 # Your awesome code begins here!
+
+import os
+import sys
+import glob
+import zipfile
+
+def dotm_search(text, directory):
+    if directory: 
+        os.chdir(directory)
+    files = glob.glob('*.dotm')
+    files_found = 0
+    print "Searching directory {} for text '{}'".format(directory, text)
+    for file in files:
+        with zipfile.ZipFile(file) as z:
+            with z.open('word/document.xml', 'r') as doc:
+                for line in doc:
+                    if text in line:
+                        print "Match found in file {}/{}".format(directory, file)
+                        print "..." + line[line.find(text) - 40:line.find(text) + 40] + "..."
+                        files_found += 1
+    print "Files searched: {}".format(len(files))
+    print "Files matched: {}".format(files_found)
+
+if __name__ == '__main__':
+    if len(sys.argv) > 2:
+        dotm_search(sys.argv[1], sys.argv[2])
+    elif len(sys.argv) > 1:
+        dotm_search(sys.argv[1], os.getcwd())
+    else: 
+        print "Please provide searchable text."
